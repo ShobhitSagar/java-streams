16cb538b6a331e92447b159b20357eccb2a8896a
Manish Giri <manish.giri.me@gmail.com>
2017-09-23 00:57:33 -0400

    Declarative filtering of a List using Streams

--- src/test/java/lectures/Lecture1.java
+++ src/test/java/lectures/Lecture1.java
@@ -6,6 +6,7 @@ import beans.Person;
 
 import com.google.common.collect.ImmutableList;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,12 +22,27 @@ public class Lecture1 {
     List<Person> people = MockData.getPeople();
     // 1. Find people aged less or equal 18
     // 2. Then change implementation to find first 10 people
+    int limit = 0;
+    List<Person> youngPeople = new ArrayList<>();
+    for(Person p: people) {
+        if(p.getAge() <= 18) {
+            limit++;
+            youngPeople.add(p);
+            if(limit == 10) {
+                break;
+            }
+        }
+    }
+
+      System.out.println(youngPeople);
+      System.out.println(youngPeople.size());
 
   }
 
   @Test
   public void declarativeApproachUsingStreams() throws Exception {
     ImmutableList<Person> people = MockData.getPeople();
-
+    List<Person> youngPeople = people.stream().filter(person -> person.getAge() <= 18).limit(10).collect(Collectors.toList());
+    youngPeople.forEach(System.out::println);
   }
 }
